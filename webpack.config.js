e72c0b36e93b55acebaf4a2432dcb8bd42014a61
Girish-Br <girishpacchu1427@gmail.com>
2020-02-14 19:18:24 +0530

    [Girish-Br]Add.started with oreo UI development

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // 1
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    // 2
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    // 3
+    devServer: {
+      contentBase: './dist'
+    }
+  };
